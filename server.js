e5bdb7cb2c42549bf8e3eb87f4e263acd4643172
Denis S Dujota <denisdujota@gmail.com>
2024-12-29 20:20:09 +0300

    finish registration flow

--- server.js
+++ server.js
@@ -6,8 +6,13 @@ const app = express();
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 
+// CONTROLLERS
+const authCtrl = require('./controllers/auth');
+
 // Set the port from environment variable or default to 3000
 const port = process.env.PORT ? process.env.PORT : '3000';
+
+// MIDDLEWARE
 
 // Middleware to parse URL-encoded data from forms
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +20,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // Morgan for logging HTTP requests
 app.use(morgan('dev'));
+
+// PUBLIC ROUTES
+app.get('/', (req, res) => {
+  res.render('index.ejs');
+});
+
+app.use('/auth', authCtrl);
+
+// PROTECTED ROUTES
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
